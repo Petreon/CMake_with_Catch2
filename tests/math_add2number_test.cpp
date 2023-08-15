6bc78438450a153c004fe98c77c3d180e2d1f219
Petreon <vitorr2009@gmail.com>
2023-08-15 17:21:14 -0300

    alterations that tests is working

--- tests/math_add2number_test.cpp
+++ tests/math_add2number_test.cpp
@@ -1,4 +1,4 @@
-#include "catch_amalgamated.hpp"
+#include <catch2/catch_test_macros.hpp>
 #include "mathslib.hpp"
 
 TEST_CASE("math add2number function", "[math.add2number()]") {
@@ -13,11 +13,3 @@ TEST_CASE("math add2number function", "[math.add2number()]") {
 
 }
 
-TEST_CASE("math multiply2numbers function", "[math.multiply2numbers()]") {
-    mathslib math_test;
-
-    SECTION("multipling numbers") {
-        REQUIRE(math_test.multply2numbers(2,6) == 12);
-        REQUIRE(math_test.multply2numbers(100,20) == 2000);
-    }
-}
